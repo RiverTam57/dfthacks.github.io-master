2c136a6bde049859938e393a818563578833db1c
RiverTam57 <hackpartners@Hacks-iMac.lan>
2017-01-26 13:18:43 +0000

    Final

--- js/main.js
+++ js/main.js
@@ -123,26 +123,19 @@ jQuery(document).ready(function($){
 			}
 		});
 	}
-
-	if ($('#back-to-top').length) {
-	    var scrollTrigger = 100, // px
-	        backToTop = function () {
-	            var scrollTop = $(window).scrollTop();
-	            if (scrollTop > scrollTrigger) {
-	                $('#back-to-top').addClass('show');
-	            } else {
-	                $('#back-to-top').removeClass('show');
-	            }
-	        };
-	    backToTop();
-	    $(window).on('scroll', function () {
-	        backToTop();
-	    });
-	    $('#back-to-top').on('click', function (e) {
-	        e.preventDefault();
-	        $('html,body').animate({
-	            scrollTop: 0
-	        }, 700);
-	    });
-	}
+});
+$(function(){
+ var shrinkHeader = 300;
+  $(window).scroll(function() {
+    var scroll = getCurrentScroll();
+      if ( scroll >= shrinkHeader ) {
+           $('.header').addClass('shrink');
+        }
+        else {
+            $('.header').removeClass('shrink');
+        }
+  });
+function getCurrentScroll() {
+    return window.pageYOffset || document.documentElement.scrollTop;
+    }
 });
